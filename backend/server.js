22e6d57204bf569b001ae0865dd51f26f56ea23d
Kavya Naik <kavyanaik2112@gmail.com>
2025-02-25 14:38:39 +0530

    changed PORT

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,11 @@ const app = express();
 
 app.use(express.json()); // allows to take json as req body
 
-app.use("/api/products", ProductRouter)
+app.use("/api/products", ProductRouter);
 
-app.listen(5000, () => {
+const appPort = process.env.PORT || 5000;
+
+app.listen(appPort, () => {
   connectDB();
-  console.log("app is listening at port ", "5000");
+  console.log("app is listening at port ", appPort);
 });
